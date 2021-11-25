af056ffed598b8239668a5187db78074d25d4af7
MatteoLiberati <matteoliberati@hotmail.com>
2021-11-25 16:46:15 +0100

    header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { AppComponent } from './app.component';
 import { NewQuoteComponent } from './new-quote/new-quote.component';
 import { QuotesListComponent } from './quotes-list/quotes-list.component';
 import { QuoteItemComponent } from './quotes-list/quote-item/quote-item.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { QuoteItemComponent } from './quotes-list/quote-item/quote-item.componen
     NewQuoteComponent,
     QuotesListComponent,
     QuoteItemComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
