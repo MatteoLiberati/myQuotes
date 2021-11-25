b57ff2f6a695e7a45cc4319a4fa7e846d92e4645
MatteoLiberati <matteoliberati@hotmail.com>
2021-11-25 15:22:28 +0100

    spinner

--- src/app/quotes-list/quotes-list.component.ts
+++ src/app/quotes-list/quotes-list.component.ts
@@ -11,12 +11,14 @@ export class QuotesListComponent implements OnInit {
 
   constructor(private quotesService: QuotesService) { }
 
+  spinner: boolean = false;
   quotes : Quote[] = [];
 
   ngOnInit(): void {
     this.quotesService.initialLoadingQuotes();
     this.quotesService.updateQuotes.subscribe(quotes=>{
       this.quotes = quotes;
+      this.spinner = true;
     })
   }
 
