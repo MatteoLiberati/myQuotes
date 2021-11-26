bcd54262f78b5bf4cc5bdab97c79d175829ccc1b
MatteoLiberati <matteoliberati@hotmail.com>
2021-11-26 14:34:43 +0100

    fix problem duplicated

--- src/app/quotes.service.ts
+++ src/app/quotes.service.ts
@@ -9,49 +9,49 @@ import { Quote } from './quote.interface';
 export class QuotesService {
   constructor(private dbConnectionsService: DbConnectionsService) {}
 
-  // quotes : Quote[] = [];
+  quotes : Quote[] = [];
   updateQuotes : Subject<Quote[]> = new Subject();
   clickCreateQuote : EventEmitter<boolean> = new EventEmitter();
   searchMode : EventEmitter<boolean> = new EventEmitter();
   noResult : EventEmitter<boolean> = new EventEmitter();
 
-  quotes : Quote[] = [
-    {
-      "quote" : "Una vera amica è colei che ti prende per mano e ti tocca il cuore." ,
-      "author" : "Gabriel Garcia Márquez",
-      "dateOfInput" : new Date('2015-07-15T05:24:00'),
-    },
-    {
-      "quote" : "Gioca e basta. Divertiti. Goditi la partita." ,
-      "author" : "Michael Jordan",
-      "dateOfInput" : new Date('2017-09-15T05:24:00'),
-    },
-    {
-      "quote" : "Un’amica è un’altra me stessa." ,
-      "author" : "Zenone di Cizio",
-      "dateOfInput" : new Date('2017-10-05T05:24:00'),
-    },
-    {
-      "quote" : "L'arte comincia dalla resistenza: dalla resistenza vinta. Non esiste capolavoro umano che non sia stato ottenuto." ,
-      "author" : "anonymous",
-      "dateOfInput" : new Date('2017-10-08T05:24:00'),
-    },
-    {
-      "quote" : "L'educazione consiste nel darci delle idee, la buona educazione nel metterle in proporzione." ,
-      "author" : "MONTESQUIEU",
-      "dateOfInput" : new Date('2020-11-10T05:24:00'),
-    },
-    {
-      "quote" : "Molti confondono una cattiva gestione con il destino" ,
-      "author" : "kin hubbard",
-      "dateOfInput" : new Date('2020-12-10T05:24:00'),
-    },
-    {
-      "quote" : "Non cercare giustificazioni per il fallimento, cercale per la vittoria." ,
-      "author" : "chi-chi rodriguez",
-      "dateOfInput" : new Date('2021-01-10T05:24:00'),
-    }
-  ];
+  // quotes : Quote[] = [
+  //   {
+  //     "quote" : "Una vera amica è colei che ti prende per mano e ti tocca il cuore." ,
+  //     "author" : "Gabriel Garcia Márquez",
+  //     "dateOfInput" : new Date('2015-07-15T05:24:00'),
+  //   },
+  //   {
+  //     "quote" : "Gioca e basta. Divertiti. Goditi la partita." ,
+  //     "author" : "Michael Jordan",
+  //     "dateOfInput" : new Date('2017-09-15T05:24:00'),
+  //   },
+  //   {
+  //     "quote" : "Un’amica è un’altra me stessa." ,
+  //     "author" : "Zenone di Cizio",
+  //     "dateOfInput" : new Date('2017-10-05T05:24:00'),
+  //   },
+  //   {
+  //     "quote" : "L'arte comincia dalla resistenza: dalla resistenza vinta. Non esiste capolavoro umano che non sia stato ottenuto." ,
+  //     "author" : "anonymous",
+  //     "dateOfInput" : new Date('2017-10-08T05:24:00'),
+  //   },
+  //   {
+  //     "quote" : "L'educazione consiste nel darci delle idee, la buona educazione nel metterle in proporzione." ,
+  //     "author" : "MONTESQUIEU",
+  //     "dateOfInput" : new Date('2020-11-10T05:24:00'),
+  //   },
+  //   {
+  //     "quote" : "Molti confondono una cattiva gestione con il destino" ,
+  //     "author" : "kin hubbard",
+  //     "dateOfInput" : new Date('2020-12-10T05:24:00'),
+  //   },
+  //   {
+  //     "quote" : "Non cercare giustificazioni per il fallimento, cercale per la vittoria." ,
+  //     "author" : "chi-chi rodriguez",
+  //     "dateOfInput" : new Date('2021-01-10T05:24:00'),
+  //   }
+  // ];
 
   addNewQuote(quote: Quote) {
     this.quotes.push(quote);
@@ -92,20 +92,16 @@ export class QuotesService {
     if(userSearch != ""){
       let strings : string[] = userSearch.toLowerCase().split(" "); 
       // double insertion control
-      for(let i= 0; i<strings.length - 1; i++){
-        if(strings[i] == strings[i+1]){
-          strings.splice(i,1);
-        }
-      }
+      strings = [...new Set(strings)];
   
       let filteredQuotes : Quote[] = [];
       for (let string of strings){
         this.quotes.map(quote=>{
           //exclude punctuation
-          let punctRE = /[\u2000-\u206F\u2E00-\u2E7F\\'!"#$%&()*+,\-.\/:;<=>?@\[\]^_`{|}~]/g;
-          
-          if(quote.author.toLowerCase().replace(punctRE, ' ').split(" ").includes(string) 
-          || quote.quote.toLowerCase().replace(punctRE, ' ').split(" ").includes(string)){
+          let punctRE = /[\u2000-\u206F\u2E00-\u2E7F\\!"#$%&()*+,\-.\/:;<=>?@\[\]^_`{|}~]/g;
+        
+          if(quote.author.toLowerCase().replace(punctRE, ' ').split(" ").includes(string.replace(punctRE, ' ')) 
+          || quote.quote.toLowerCase().replace(punctRE, ' ').split(" ").includes(string.replace(punctRE, ' '))){
             filteredQuotes.push(quote);
           }
         })
