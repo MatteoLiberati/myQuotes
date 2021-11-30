2d4d64484a8b99355557632a7fa0d0c7aa2a4568
MatteoLiberati <matteoliberati@hotmail.com>
2021-11-30 11:06:52 +0100

    setup initializequotes in anorher file

--- src/app/quotes.service.ts
+++ src/app/quotes.service.ts
@@ -2,6 +2,8 @@ import { EventEmitter, Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 import { DbConnectionsService } from './db-connections.service';
 import { Quote } from './quote.interface';
+// initial setup quotes - associate with the variable quotes
+import { initializeQuotes } from "../assets/initializeQuotes";
 
 @Injectable({
   providedIn: 'root',
@@ -18,51 +20,6 @@ export class QuotesService {
   searchMode : EventEmitter<boolean> = new EventEmitter();
   noResult : EventEmitter<boolean> = new EventEmitter();
   menuMobile : EventEmitter<boolean> = new EventEmitter();
-
-  // quotes : Quote[] = [
-  //   {
-  //     "quote" : "Una vera amica è colei che ti prende per mano e ti tocca il cuore." ,
-  //     "author" : "Gabriel Garcia Márquez",
-  //     "dateOfInput" : new Date('2015-07-15T05:24:00'),
-  //     "id" : 0,
-  //   },
-  //   {
-  //     "quote" : "Gioca e basta. Divertiti. Goditi la partita." ,
-  //     "author" : "Michael Jordan",
-  //     "dateOfInput" : new Date('2017-09-15T05:24:00'),
-  //     "id" : 1,
-  //   },
-  //   {
-  //     "quote" : "Un’amica è un’altra me stessa." ,
-  //     "author" : "Zenone di Cizio",
-  //     "dateOfInput" : new Date('2017-10-05T05:24:00'),
-  //     "id" : 2,
-  //   },
-  //   {
-  //     "quote" : "L'arte comincia dalla resistenza: dalla resistenza vinta. Non esiste capolavoro umano che non sia stato ottenuto." ,
-  //     "author" : "anonymous",
-  //     "dateOfInput" : new Date('2017-10-08T05:24:00'),
-  //     "id" : 3,
-  //   },
-  //   {
-  //     "quote" : "L'educazione consiste nel darci delle idee, la buona educazione nel metterle in proporzione." ,
-  //     "author" : "MONTESQUIEU",
-  //     "dateOfInput" : new Date('2020-11-10T05:24:00'),
-  //     "id" : 4,
-  //   },
-  //   {
-  //     "quote" : "Molti confondono una cattiva gestione con il destino" ,
-  //     "author" : "kin hubbard",
-  //     "dateOfInput" : new Date('2020-12-10T05:24:00'),
-  //     "id" : 5,
-  //   },
-  //   {
-  //     "quote" : "Non cercare giustificazioni per il fallimento, cercale per la vittoria." ,
-  //     "author" : "chi-chi rodriguez",
-  //     "dateOfInput" : new Date('2021-01-10T05:24:00'),
-  //     "id" : 6,
-  //   }
-  // ];
 
   addNewQuote(quote: Quote) {
     this.quotes.push(quote);
---
+++ src/assets/initializeQuotes.ts
@@ -0,0 +1,44 @@
+  export let initializeQuotes = [
+    {
+      "quote" : "Una vera amica è colei che ti prende per mano e ti tocca il cuore." ,
+      "author" : "Gabriel Garcia Márquez",
+      "dateOfInput" : new Date('2015-07-15T05:24:00'),
+      "id" : 0,
+    },
+    {
+      "quote" : "Gioca e basta. Divertiti. Goditi la partita." ,
+      "author" : "Michael Jordan",
+      "dateOfInput" : new Date('2017-09-15T05:24:00'),
+      "id" : 1,
+    },
+    {
+      "quote" : "Un’amica è un’altra me stessa." ,
+      "author" : "Zenone di Cizio",
+      "dateOfInput" : new Date('2017-10-05T05:24:00'),
+      "id" : 2,
+    },
+    {
+      "quote" : "L'arte comincia dalla resistenza: dalla resistenza vinta. Non esiste capolavoro umano che non sia stato ottenuto." ,
+      "author" : "anonymous",
+      "dateOfInput" : new Date('2017-10-08T05:24:00'),
+      "id" : 3,
+    },
+    {
+      "quote" : "L'educazione consiste nel darci delle idee, la buona educazione nel metterle in proporzione." ,
+      "author" : "MONTESQUIEU",
+      "dateOfInput" : new Date('2020-11-10T05:24:00'),
+      "id" : 4,
+    },
+    {
+      "quote" : "Molti confondono una cattiva gestione con il destino" ,
+      "author" : "kin hubbard",
+      "dateOfInput" : new Date('2020-12-10T05:24:00'),
+      "id" : 5,
+    },
+    {
+      "quote" : "Non cercare giustificazioni per il fallimento, cercale per la vittoria." ,
+      "author" : "chi-chi rodriguez",
+      "dateOfInput" : new Date('2021-01-10T05:24:00'),
+      "id" : 6,
+    }
+  ];
